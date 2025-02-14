94397ff65c68fec1725135442667cbf347d59107
notPlancha <andre.plancha@hotmail.com>
2025-02-14 15:03:59 +0100

    first commit

---
+++ TDD_lab_student/Task1_Unittest_rover.py
@@ -0,0 +1,44 @@
+'''
+    Student shall write their names here
+        1. Student 1
+        2. Student 2
+'''
+
+
+import unittest
+from Task1_Rover import rovar
+
+class test_string(unittest.TestCase):
+    '''
+        _LOWER_CONSTANTS = "bcdfhjklmnpqrstvwxz"
+        _UPPER_CONSTANTS = "BCFGHJKLMNPQRSTVWXZ"
+        Swedish vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö']
+
+        Write your TCs based on the lab instructions. One TC has been written below as an example
+        
+    '''
+
+    @classmethod
+    def setUpClass(cls):
+        '''
+            Set up shared resources = Class instance to access rover class methods
+        '''
+        cls.rv= rovar() 
+
+
+    # Example test case to check lower case rover
+    def test_enrove_small(self):
+        self.assertEqual(self.rv.enrove('b'), 'bob')
+
+    # You can continue writing your test cases here based on the assignment description
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print("***********START OF All TEST CASES RESULTS SHOWN BELOW**************")
+    unittest.main(verbosity = 2)
---
+++ TDD_lab_student/Task2.py
@@ -0,0 +1,14 @@
+import re
+from collections import Counter
+
+class TextProcessor:
+    def __init__(self, text):
+        self.text = text
+    '''
+    The TC for User story 1 will fail as there is no implementation added. Add the implementation of your user stories below.
+    '''
+    def convert_to_lowercase(self):
+        '''
+            write code to convert the text to the lower case
+        '''
+        pass
