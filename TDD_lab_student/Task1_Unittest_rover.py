ce0f1951fb9c2a3fae764acc08533b0164991bd7
notPlancha <31565873+notPlancha@users.noreply.github.com>
2025-02-18 23:42:43 +0100

    linter and formater

--- TDD_lab_student/Task1_Unittest_rover.py
+++ TDD_lab_student/Task1_Unittest_rover.py
@@ -1,44 +1,37 @@
-'''
-    Student shall write their names here
-        1. Student 1
-        2. Student 2
-'''
-
+"""
+Student shall write their names here
+    1. Student 1
+    2. Student 2
+"""
 
 import unittest
 from Task1_Rover import rovar
 
+
 class test_string(unittest.TestCase):
-    '''
-        _LOWER_CONSTANTS = "bcdfhjklmnpqrstvwxz"
-        _UPPER_CONSTANTS = "BCFGHJKLMNPQRSTVWXZ"
-        Swedish vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö']
+  """
+  _LOWER_CONSTANTS = "bcdfhjklmnpqrstvwxz"
+  _UPPER_CONSTANTS = "BCFGHJKLMNPQRSTVWXZ"
+  Swedish vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö']
 
-        Write your TCs based on the lab instructions. One TC has been written below as an example
-        
-    '''
+  Write your TCs based on the lab instructions. One TC has been written below as an example
 
-    @classmethod
-    def setUpClass(cls):
-        '''
-            Set up shared resources = Class instance to access rover class methods
-        '''
-        cls.rv= rovar() 
+  """
 
+  @classmethod
+  def setUpClass(cls):
+    """
+    Set up shared resources = Class instance to access rover class methods
+    """
+    cls.rv = rovar()
 
-    # Example test case to check lower case rover
-    def test_enrove_small(self):
-        self.assertEqual(self.rv.enrove('b'), 'bob')
+  # Example test case to check lower case rover
+  def test_enrove_small(self):
+    self.assertEqual(self.rv.enrove("b"), "bob")
 
-    # You can continue writing your test cases here based on the assignment description
-
+  # You can continue writing your test cases here based on the assignment description
 
 
-
-
-
-
-
-if __name__ == '__main__':
-    print("***********START OF All TEST CASES RESULTS SHOWN BELOW**************")
-    unittest.main(verbosity = 2)
+if __name__ == "__main__":
+  print("***********START OF All TEST CASES RESULTS SHOWN BELOW**************")
+  unittest.main(verbosity=2)
--- TDD_lab_student/Task2_unittest.py
+++ TDD_lab_student/Task2_unittest.py
@@ -1,39 +1,40 @@
-'''
-    Student shall write their names here
-        1. Student 1
-        2. Student 2
-'''
+"""
+Student shall write their names here
+    1. Student 1
+    2. Student 2
+"""
 
 import unittest
 from Task2 import TextProcessor
 
+
 class TestTextProcessor(unittest.TestCase):
-    tp = None
-    def setUp(self):
-        """
-        You can start with the sample text below but please feel free to add more text to cover all scenarios. 
-        """
-        self.sample_text = "Hello! This is a sample text 1. Contact me at user@example.com. Python is awesome. The Python programming language is widely used. #Python #NLP Check out https://example.com."
-        global tp 
-        tp = TextProcessor(self.sample_text)
+  tp = None
 
-
-
+  def setUp(self):
     """
+    You can start with the sample text below but please feel free to add more text to cover all scenarios.
+    """
+    self.sample_text = "Hello! This is a sample text 1. Contact me at user@example.com. Python is awesome. The Python programming language is widely used. #Python #NLP Check out https://example.com."
+    global tp
+    tp = TextProcessor(self.sample_text)
+
+  """
         One example case test case is provided below but the implmentation of the function is missing to get you
         going with the TDD. Please note the test case will fail without implementation of the function in task2.py. 
         You will see an assertion error AssertionError: None != 'hello! this is a sample text 1. contact [130 chars]com.'. 
         The error will go away once you implement the function.
     """
-    #TC for User story no 1 - This will fail!
-    def test_convert_to_lowercase(self):
-        self.assertEqual(tp.convert_to_lowercase(), self.sample_text.lower())
+
+  # TC for User story no 1 - This will fail!
+  def test_convert_to_lowercase(self):
+    self.assertEqual(tp.convert_to_lowercase(), self.sample_text.lower())
 
 
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
-    
-    """
+if __name__ == "__main__":
+  unittest.main(verbosity=2)
+
+  """
     To generate an HTML report
     coverage run -a --branch Task1_Unittest_rover.py 
     coverage run -a --branch Task2_unittest.py 
