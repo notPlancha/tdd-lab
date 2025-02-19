0c4fe95b17cc32754854c316b4b915cd7b777105
notPlancha <andre.plancha@hotmail.com>
2025-02-19 14:08:51 +0100

    setup task 2

--- TDD_lab_student/Task2.py
+++ TDD_lab_student/Task2.py
@@ -7,11 +7,45 @@ class TextProcessor:
     self.text = text
 
   """
-    The TC for User story 1 will fail as there is no implementation added. Add the implementation of your user stories below.
-    """
+  The TC for User story 1 will fail as there is no implementation added. Add the implementation of your user stories below.
+  """
 
-  def convert_to_lowercase(self):
-    """
-    write code to convert the text to the lower case
-    """
+  def convert_to_lowercase(self) -> str:
+    """Convert all words to lowercase."""
+    pass
+
+  def extract_emails(self) -> list:
+    """Find and extract all email addresses from the document."""
+    pass
+
+  def count_hashtags(self) -> int:
+    """Find and count all unique hashtags (words or phrases starting with #) used in the document."""
+    pass
+
+  def extract_links(self) -> list:
+    """Identify and list all URLs mentoned in the text."""
+    pass
+
+  def avg_word_length(self) -> float:
+    """Calculate the average word length in the document."""
+    pass
+
+  def top_words(self, n: int = 3) -> list[str]:
+    """Find and list the top n most frequent words in the document."""
+    pass
+
+  def longest_word(self) -> str:
+    """Find and return the longest word in the document."""
+    pass
+
+  def identify_sentences(self, word="Python") -> list[str]:
+    """Find and list all sentences containing the specified word."""
+    pass
+
+  def remove_special(self) -> str:
+    """Remove all punctuation and special characters from the document."""
+    pass
+
+  def num_to_words(self) -> str:
+    """Convert all numerical figures (1-10) within the text into their respective written-out forms (for example, "This is sample text 1, 2, 3" shall become "This is sample text one, two, three")."""
     pass
--- TDD_lab_student/Task2_unittest.py
+++ TDD_lab_student/Task2_unittest.py
@@ -1,7 +1,7 @@
 """
 Student shall write their names here
-    1. Student 1
-    2. Student 2
+  1. André Plancha
+  2. Yana Zlatanova
 """
 
 import unittest
@@ -9,35 +9,56 @@ from Task2 import TextProcessor
 
 
 class TestTextProcessor(unittest.TestCase):
-  tp = None
-
   def setUp(self):
-    """
-    You can start with the sample text below but please feel free to add more text to cover all scenarios.
-    """
+    # TODO consider other sample texts
     self.sample_text = "Hello! This is a sample text 1. Contact me at user@example.com. Python is awesome. The Python programming language is widely used. #Python #NLP Check out https://example.com."
-    global tp
-    tp = TextProcessor(self.sample_text)
+    self.tp = TextProcessor(self.sample_text)
 
   """
-        One example case test case is provided below but the implmentation of the function is missing to get you
-        going with the TDD. Please note the test case will fail without implementation of the function in task2.py. 
-        You will see an assertion error AssertionError: None != 'hello! this is a sample text 1. contact [130 chars]com.'. 
-        The error will go away once you implement the function.
-    """
+  One example case test case is provided below but the implmentation of the function is missing to get you
+  going with the TDD. Please note the test case will fail without implementation of the function in task2.py. 
+  You will see an assertion error AssertionError: None != 'hello! this is a sample text 1. contact [130 chars]com.'. 
+  The error will go away once you implement the function.
+  """
 
   # TC for User story no 1 - This will fail!
   def test_convert_to_lowercase(self):
-    self.assertEqual(tp.convert_to_lowercase(), self.sample_text.lower())
+    self.assertEqual(self.tp.convert_to_lowercase(), self.sample_text.lower())
+
+  def test_extract_emails(self):
+    pass
+
+  def test_count_hashtags(self):
+    pass
+
+  def test_extract_links(self):
+    pass
+
+  def test_avg_word_length(self):
+    pass
+
+  def test_top_words(self):
+    pass
+
+  def test_longest_word(self):
+    pass
+
+  def test_identify_sentences(self):
+    pass
+
+  def test_remove_special(self):
+    pass
+
+  def test_num_to_words(self):
+    pass
 
 
 if __name__ == "__main__":
   unittest.main(verbosity=2)
-
   """
-    To generate an HTML report
-    coverage run -a --branch Task1_Unittest_rover.py 
-    coverage run -a --branch Task2_unittest.py 
-    coverage report -m
-    coverage html
-    """
+  To generate an HTML report
+  coverage run -a --branch Task1_Unittest_rover.py 
+  coverage run -a --branch Task2_unittest.py 
+  coverage report -m
+  coverage html
+  """
